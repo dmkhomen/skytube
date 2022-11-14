5f909315044214afbbcaf404370adf9b7cb27f61
dmkhomen <dmkhomen@gmail.com>
2022-11-14 13:20:55 +0200

    search bar

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,7 +12,7 @@ import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
 
-export const logo = "https://i.ibb.co/HnMmVh3/skytube.png";
+export const logo = "https://i.ibb.co/160JGQ9/skytube2.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
