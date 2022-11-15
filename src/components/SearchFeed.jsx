0fa10513766aa8af892f7833984106c8811e4dba
dmkhomen <dmkhomen@gmail.com>
2022-11-15 10:22:35 +0200

    search bar + search feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {useState, useEffect } from 'react'
+import { Box, Typography } from "@mui/material"
+import { Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { useParams } from 'react-router-dom'
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([])
+  const {searchTerm} = useParams()
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => (setVideos(data.items)))
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: "auto", height: "90vh", flex: 2}}>
+      <Typography variant="h4" fontWeight="bold" sx={{color: "black", mb: 2}}>
+         Search results for: <span style={{color: "#0284c7"}}>{searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos}/>
+    </Box>
   )
 }
 
