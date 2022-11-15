2e5c0ebfa9f24e06d191fad1c1d8948e2dad49f8
dmkhomen <dmkhomen@gmail.com>
2022-11-15 11:51:57 +0200

    card width fix

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,12 +6,12 @@ import {demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChan
 
 const VideoCard = ({video: {id: { videoId }, snippet}}) => {
   return (
-    <Card sx={{width: {md: "320px", xs: "100%"}}}>
+    <Card sx={{width: { xs: "100%", sm: "358px", md: "320px"}}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{width: 358, height: 180}}
+          sx={{width: {xs: "100%", sm: "358px", md: "320px"}, height: 180}}
         />
       </Link>
       <CardContent sx={{height: "106px"}}>
